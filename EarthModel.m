8bfe6c4a4f7877cc379b689ed73826e589b45471
Aaron Abeyta <41973830+AbeytaA@users.noreply.github.com>
2018-08-06 23:48:54 -0700

    WE GOT WORKING PLANETS BOI.

--- EarthModel.m
+++ EarthModel.m
@@ -2,32 +2,31 @@
 % NAME: Akhil Avula, Aaron Abeyta
 
 clear all; close all; clc;
-T = 1; %Number of Earth years to be modelled. 
+T = 10; %Number of Earth years to be modelled. 
+t = 0:365*T; %Increment of days
 a_E = 1.496e8; %km
 e_E = 0.0167; % unitless
-b_E = a_E*sqrt(1-e_E^2);
-nu = 0:(2*pi)/365*T:(2*pi)*T;
+b_E = a_E*sqrt(1-e_E^2); %semiminor axis
+nu = 0:(2*pi)/365*T:(2*pi);
 a_M = 2.2792e8;
 e_M = 0.0935;
 b_M = a_M*sqrt(1-e_M^2);
+omegaE = 2*pi/365.2;
+omegaM = 2*pi/687;
+nuE = omegaE*t;
+nuM = omegaM*t;
+r_E = (a_E.*(1-e_E.^2))./(1+e_E.*cos(nuE));
+r_M= (a_M.*(1-e_M.^2))./(1+e_M.*cos(nuM));
 
-r_E = (a_E.*(1-e_E.^2))./(1+e_E.*cos(nu));
-r_M= (a_M.*(1-e_M.^2))./(1+e_M.*cos(nu));
+r_posE = [r_E.*cos(nuE); r_E.*sin(nuE)];
 
-r_posE = [r_E.*cos(nu); r_E.*sin(nu)];
+r_posM = [r_M.*cos(nuM); r_M.*sin(nuM)];
 
-r_posM = [r_M.*cos(nu); r_M.*sin(nu)];
+[xE,yE] = pol2cart(nuE,r_E); %Plots the actual ellipses
+[xM,yM] = pol2cart(nuM,r_M);
 
-xE = -a_E:1e5:a_E;
-y_posE = b_E./a_E.*sqrt(a_E.^2-xE.^2);
-y_negE = -b_E./a_E.*sqrt(a_E.^2-xE.^2);
-
-xM = -a_M:1e5:a_M;
-y_posM = b_M./a_M.*sqrt(a_M.^2-xM.^2);
-y_negM = -b_M./a_M.*sqrt(a_M.^2-xM.^2);
 figure(1)
-while true
-for k = 1:length(nu)
+for k = 1:length(t)
     f = r_posE(:,k);
     plotv(f,'o-')
     hold on 
@@ -37,18 +36,12 @@ for k = 1:length(nu)
     axis square
     xlim([-3e8 3e8])
     ylim([-3e8 3e8])
-    plot(xE,y_posE,'b')
-    plot(xE,y_negE,'b')
-    plot(xM,y_posM,'r')
-    plot(xM,y_negM,'r')
+    plot(xE,yE, 'b')
+    plot(xM,yM, 'r')
     drawnow
     hold off
     
 end 
-
-end 
-
-
 
 
 
