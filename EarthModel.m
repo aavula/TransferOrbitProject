15cd2ded4ac061f994511d4e726e3d5ca7518ccc
Aaron Abeyta <41973830+AbeytaA@users.noreply.github.com>
2018-08-12 11:02:05 -0700

    Work in progress with getting transfer orbits
    Magenta line is Mars at nu_2, with the red being Mars at its relative position to earth at the start. Transfer orbit is acting weird.

--- EarthModel.m
+++ EarthModel.m
@@ -2,48 +2,74 @@
 % NAME: Akhil Avula, Aaron Abeyta
 
 clear all; close all; clc;
-T = 10; %Number of Earth years to be modelled. 
+T = 3; %Number of Earth years to be modelled. 
 t = 0:365*T; %Increment of days
 a_E = 1.496e8; %km
 e_E = 0.0167; % unitless
 b_E = a_E*sqrt(1-e_E^2); %semiminor axis
-nu = 0:(2*pi)/365*T:(2*pi);
 a_M = 2.2792e8;
 e_M = 0.0935;
 b_M = a_M*sqrt(1-e_M^2);
+mu = 6.67408e-11*1.989e30*(1e-9/1.33959e-10);
+lambda = 4*pi^2/mu;
 omegaE = 2*pi/365.2;
 omegaM = 2*pi/687;
 nuE = omegaE*t;
 nuM = omegaM*t;
+nuM2 = omegaM*t+pi./4; %pi/4 is an arbitary phase difference between starting earth and end mars.
 r_E = (a_E.*(1-e_E.^2))./(1+e_E.*cos(nuE));
-r_M= (a_M.*(1-e_M.^2))./(1+e_M.*cos(nuM));
+r_M = (a_M.*(1-e_M.^2))./(1+e_M.*cos(nuM));
+r_M2= (a_M.*(1-e_M.^2))./(1+e_M.*cos(nuM2));
 
 r_posE = [r_E.*cos(nuE); r_E.*sin(nuE)];
 
 r_posM = [r_M.*cos(nuM); r_M.*sin(nuM)];
 
+r_posM2 = [r_M2.*cos(nuM2); r_M2.*sin(nuM2)];
+
 [xE,yE] = pol2cart(nuE,r_E); %Plots the actual ellipses
 [xM,yM] = pol2cart(nuM,r_M);
+%deltaT = zeros(1:length(t));
 
 figure(1)
 for k = 1:length(t)
+    E_M = acos((e_M+cos(nuM(k)))/(1+e_M*cos(nuM(k))));
+    E_M2 = acos((e_M+cos(nuM2(k)))/(1+e_M*cos(nuM2(k))));
     f = r_posE(:,k);
-    plotv(f,'o-')
-    hold on 
     g = r_posM(:,k);
-    plotv(g,'o-')
+    h = r_posM2(:,k);
+    if nuM2(k) < nuM(k)
+    deltaT = abs(sqrt(a_M.^3./mu).*(2*pi + E_M2-e_M.*sin(E_M2) - (E_M - e_M.*sin(E_M)))); 
+    else 
+    deltaT = abs(sqrt(a_M.^3./mu).*(E_M2-e_M.*sin(E_M2) - (E_M - e_M.*sin(E_M)))); 
+    end 
+    a_s = ((2*pi*deltaT./((nuM2(k) - nuE(k)))).^2./lambda).^(1/3);
+    e_s = (r_E(k).*cos(nuE(k) - sqrt(r_E(k).^2.*cos(nuE(k)).^2+4.*a_s.*(abs(a_s-r_E(k)))))/(-2.*a_s));
+    
+    if nuE(k) < nuM2(k)
+    nus = nuE(k):.0001:nuM2(k);
+    elseif nuE(k) > nuM2(k)
+        nus = nuM2(k):.0001:nuE(k);
+    else 
+        continue 
+    end 
+        
+    r_s = (a_s.*(1-e_s.^2))./(1+e_s.*cos(nus));
+    [xs,ys] = pol2cart(nus,r_s);
+    plotv(f,'bo-')
+    hold on 
+    plotv(g,'ro-')
+    plotv(h,'mo-')
     grid
     axis square
     xlim([-3e8 3e8])
     ylim([-3e8 3e8])
     plot(xE,yE, 'b')
     plot(xM,yM, 'r')
+    plot(xs,ys)
+
     drawnow
     hold off
     
-end 
-
-
-
-
-
+end
+    
