b765d0dca2da94ec3ce30a8671cf7442d51b3d59
Aaron Abeyta <41973830+AbeytaA@users.noreply.github.com>
2018-08-13 11:38:52 -0700

    Update EarthModel.m

--- EarthModel.m
+++ EarthModel.m
@@ -2,7 +2,7 @@
 % NAME: Akhil Avula, Aaron Abeyta
 
 clear all; close all; clc;
-T = 3; %Number of Earth years to be modelled. 
+T = .5; %Number of Earth years to be modelled. 
 t = 0:365*T; %Increment of days
 a_E = 1.496e8; %km
 e_E = 0.0167; % unitless
@@ -31,13 +31,30 @@ r_posM2 = [r_M2.*cos(nuM2); r_M2.*sin(nuM2)];
 [xM,yM] = pol2cart(nuM,r_M);
 %deltaT = zeros(1:length(t));
 
+
+deltaT = 30*6;
+E_2s = zeros(1,length(t));
+fnuE = zeros(1,length(t));
+fnuM = zeros(1,length(t));
+E_M = zeros(1,length(t));
+tic 
+for j = 1:length(t)
+syms E_2
+fnuE(j) = abs(nuE(j)/(2.*pi) - fix(nuE(j)./(2.*pi)).*2.*pi);
+fnuM(j) = abs(nuM(j)/(2.*pi) - fix(nuM(j)./(2.*pi)).*2.*pi);
+    
+E_M(j) = acos((e_M+cos(fnuM(j)))/(1+e_M*cos(fnuM(j))));
+
+E_2s(j) = double(vpasolve(E_2 - e_M.*sin(E_2) == deltaT./sqrt(a_M^3/mu) - 2.*0.*pi + E_M(j) - e_M.*sin(E_M(j)),E_2 ));
+
+end 
+toc   
+    
+  
 figure(1)
+while true 
 for k = 1:length(t)
-    fnuE = abs(nuE(k)/(2.*pi) - fix(nuE(k)./(2.*pi)).*2.*pi);
-    fnuM = abs(nuM(k)/(2.*pi) - fix(nuM(k)./(2.*pi)).*2.*pi);
     
-    
-    E_M = acos((e_M+cos(fnuM))/(1+e_M*cos(fnuM)));
     %E_M2 = acos((e_M+cos(nuM2(k)))/(1+e_M*cos(nuM2(k))));
     f = r_posE(:,k);
     g = r_posM(:,k);
@@ -48,15 +65,12 @@ for k = 1:length(t)
     %deltanu = fnuM - fnuE;
     %deltanu2 = fnuM2- fnuE;
    
-    deltaT = 30*6;
-    syms E_2
-    E_2s = double(vpasolve(E_2 - e_M.*sin(E_2) == deltaT./sqrt(a_M^3/mu) - 2.*0.*pi + E_M - e_M.*sin(E_M),E_2 ));
-    
-    nuM2 = acos((a_M.*cos(E_2s)-a_M.*e_M)/r_M(k));
+   
+    nuM2 = acos((a_M.*cos(E_2s(k))-a_M.*e_M)/r_M(k));
     
     fnuM2 = (nuM2/(2.*pi) - fix(nuM2./(2.*pi)).*2.*pi);
     
-    deltanu2 = (nuM2-nuM(k));
+    deltanu2 = (nuM2-nuE(k));
     %if nuM2(k) < nuM(k)
     %deltaT = abs(sqrt(a_M.^3./mu).*(2*pi + E_M2-e_M.*sin(E_M2) - (E_M - e_M.*sin(E_M)))); 
     %else 
@@ -64,20 +78,23 @@ for k = 1:length(t)
     %end 
     a_s = ((2*pi*deltaT./deltanu2).^2./lambda).^(1/3);
     
-    e_s1 = r_E(k).*cos(fnuE); %-b
-    e_s2 = r_E(k).^2.*cos(fnuE).^2; %b^2
+    e_s1 = r_E(k).*cos(fnuE(k)); %-b
+    e_s2 = r_E(k).^2.*cos(fnuE(k)).^2; %b^2
     e_s3 = 4.*a_s.*(a_s-r_E(k)); %4ac
     e_s4 = -2.*a_s.^2; %2a
     e_s = (e_s1 - sqrt(e_s2+e_s3))/e_s4;
     
     
-   if fnuE < fnuM
-    nus = fnuE:2*pi/365:fnuM;
+   if fnuE(k) < fnuM(k)
+    nus = fnuE(k):.00001:fnuM(k);
+   elseif fnuE(k) > fnuM(k)
+       nus = fnuM(k):.00001:fnuE(k);
    else 
-       nus = fnuM:2*pi/365:fnuE;
+       continue
    end 
-        
+       
     r_s = (a_s.*(1-e_s.^2))./(1+e_s.*cos(nus));
+
     [xs,ys] = pol2cart(nus,r_s);
     plotv(f,'bo-')
     hold on 
@@ -95,4 +112,6 @@ for k = 1:length(t)
     hold off
     
 end
+end 
+
     
